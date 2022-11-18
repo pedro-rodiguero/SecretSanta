aca2460e21695a08dced1aab3645f179fcd93821
pemiinem <190139323@aluno.unb.br>
2022-11-18 18:12:08 -0300

    first commit for a Secret Santa program

---
+++ main.py
@@ -0,0 +1,23 @@
+
+
+
+class SecretSanta:
+    
+    def __init__(self):
+        self.info = dict()
+        self.selections = dict()
+        self.participants = 0
+    
+    def getInfo(self):
+        self.participants = input("How many people are participating in Secret Santa?")
+    
+    def assign(self):
+        pass
+    
+    def send_email(self):
+        pass
+    
+    def start(self):
+        pass
+    
+    
